179528c58d5203558a2cba45e6a4817e0348f434
Ahmed_Elshenawy <elshenawy1ahmed@gmail.com>
2024-04-21 18:23:08 +0200

    intialCommit

---
+++ WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        CompContext context;
+        public EmployeeController()
+        {
+            context = new CompContext();
+        }
+        [HttpPost]
+        public IActionResult Add(Employee emp)
+        {
+            context.Employees.Add(emp);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Employee> emps =  context.Employees.ToList();
+            return Ok(emps);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            Employee emps =  context.Employees.FirstOrDefault(e => e.Id == id);
+            return Ok(emps);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Employee newEmp) // newemp = id =  3  , name 'maker' 
+        {
+            Employee oldEmp = context.Employees.FirstOrDefault(e => e.Id == newEmp.Id);
+            if(oldEmp is not null)
+            {
+                 oldEmp.Name = newEmp.Name;
+                 context.SaveChanges();
+            }
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Employee e = context.Employees.FirstOrDefault(e => e.Id == id);
+            if(e is not null)
+            {
+                context.Employees.Remove(e);
+                context.SaveChanges();
+            }
+            return Ok();
+        }
+        
+    }
+}
---
+++ WebAPI/Models/CompContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class CompContext : DbContext
+    {
+        public CompContext()
+        {
+            
+        }
+        public CompContext(DbContextOptions options)  : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=MyComp;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
